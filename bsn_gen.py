00fac51ef0d63f77d9b661f8751ff4f77f27c315
kadirkzlpnr <79916868+kadirkzlpnr@users.noreply.github.com>
2021-03-23 11:30:38 +0100

    Update bsn_gen.py

--- bsn_gen.py
+++ bsn_gen.py
@@ -4,18 +4,18 @@ numbers = "0123456789"
 
 while 1:
     try:
-        num_count = int(input("Hoeveel 'burgerservicenummers' wil je genereren? : "))
-        for a in range(0,num_count):
+        num_count = int(input("Hoeveel 'burgerservicenummers' wil je genereren? : ")) 
+        for a in range(0,num_count): # aantal opgevraagde bsn-nummers
             bsn_num = ""
-            for a in range(0, 9):
-                num_char = random.choice(numbers)
+            for a in range(0, 9): # lengte van de bsn-nummer 
+                num_char = random.choice(numbers) # willekeurige cijfers van 0 t/m 9 om te genereren
                 bsn_num      = bsn_num + num_char
             print("Je gegenereerde burgerservicenummer is : ", bsn_num)
-    except ValueError as e:
+    except ValueError as e: # fout handeling
         print("Fout: Controlleer of je laatste wijziging!", e)
     
-    except NameError as e: 
+    except NameError as e: # fout handeling
         print("Fout: Controlleer of je de juiste functie naam hebt gebruikt!", e)
 
-    except SyntaxError as e:
+    except SyntaxError as e: # fout handeling
         print("Fout: Controlleer op typfouten of onjuiste wijzigingen!", e)
